297bc734721173456846bb362cc13fdbc1d6cce5
david randolph <david.randolph@hashicorp.com>
2024-12-04 16:08:02 -0600

    testing

--- rg/main.tf
+++ rg/main.tf
@@ -1,7 +1,10 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+locals {
+  location = replace(var.resource_group_location, " ", "")
+}
 resource "azurerm_resource_group" "example" {
-  name     = "${var.resource_group_name}-${replace(var.resource_group_location, ' ', '')}"
+  name     = "${var.resource_group_name}-${local.location}"
   location = var.resource_group_location
 }
